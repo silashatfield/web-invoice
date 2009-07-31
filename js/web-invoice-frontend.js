e658af4c8a0aa2ffbf5a18f5b20b4e70d801a96d
mohanjith <mohanjith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-31 21:45:36 +0000

    Releasing 1.9.2
    git-svn-id: https://plugins.svn.wordpress.org/web-invoice/trunk@141556 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/web-invoice-frontend.js
+++ js/web-invoice-frontend.js
@@ -1,9 +1,9 @@
 jQuery(document).ready( function() {
-	jQuery(".payment_form").hide();
+	if (_web_invoice_method_count > 1) {
+		jQuery(".payment_form").hide();
+	}
 	jQuery("#payment_methods a").click( function() {
-		if (_web_invoice_method_count > 1) {
-			jQuery(".payment_form").hide();
-		}
+		jQuery(".payment_form").hide();
 		jQuery(jQuery(this).attr("href")).show();
 	});
 });
