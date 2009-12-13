d98f72d33642d89914bd4114605aa5a9284c0993
mohanjith <mohanjith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-13 09:10:53 +0000

    Releasing 1.10.2
    git-svn-id: https://plugins.svn.wordpress.org/web-invoice/trunk@182261 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- web-invoice.php
+++ web-invoice.php
@@ -36,7 +36,6 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
 define("WEB_INVOICE_VERSION_NUM", "1.9.14");
 define("WEB_INVOICE_TRANS_DOMAIN", "web-invoice");
 
