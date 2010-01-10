8322975e3931bac38f5227974d692ab6189c9f2d
mohanjith <mohanjith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-10 15:21:30 +0000

    Small change
    git-svn-id: https://plugins.svn.wordpress.org/web-invoice/trunk@192252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- web-invoice.php
+++ web-invoice.php
@@ -36,7 +36,7 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-define("WEB_INVOICE_VERSION_NUM", "1.9.14");
+define("WEB_INVOICE_VERSION_NUM", "1.10.10");
 define("WEB_INVOICE_TRANS_DOMAIN", "web-invoice");
 
 require_once("Flow.php");
